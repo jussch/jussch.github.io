ba4b823ee45cf7b2588a1f2b3811a74fd7351413
Justin <jaschultz25@gmail.com>
2015-01-21 16:47:54 -0500

    Adds smooth scrolling and various more additions

---
+++ javascript/on-load-script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	$(window).bind('scroll', function(event) {
+		var $target = $(event.currentTarget);
+		var scrolledY = $target.scrollTop();
+		$('.parallax').css('background-position', 'center -'+(scrolledY * 0.2)+'px');
+	});
+	$('a[href^="#"]').click(function(event) {
+		var id = $(this).attr("href");
+		var offset = 20;
+		var target = $(id).offset().top - offset;
+		 
+		$('html, body').animate({scrollTop:target}, 500);
+		event.preventDefault();
+	});
+	$.srSmoothscroll({
+      // defaults
+      step: 55,
+      speed: 100,
+      ease: 'swing'
+  });
+})
