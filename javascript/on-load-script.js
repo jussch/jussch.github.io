fa2f88f448a4a2dd43e2ac5cc2b07aa023b07ee7
Justin <jaschultz25@gmail.com>
2017-05-12 08:59:11 -0600

    New Resume, 2017. Removes background image, replaces with blue-grey gradient.

--- javascript/on-load-script.js
+++ javascript/on-load-script.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
-	$(window).bind('scroll', function(event) {
-		var $target = $(event.currentTarget);
-		var scrolledY = $target.scrollTop();
-		$('.parallax').css('background-position', 'center -'+(scrolledY * 0.2)+'px');
-	});
+	// $(window).bind('scroll', function(event) {
+	// 	var $target = $(event.currentTarget);
+	// 	var scrolledY = $target.scrollTop();
+	// 	$('.parallax').css('background-position', 'center -'+(scrolledY * 0.2)+'px');
+	// });
 	$('a[href^="#"]').click(function(event) {
 		var id = $(this).attr("href");
 		var offset = 20;
 		var target = $(id).offset().top - offset;
-		 
+
 		$('html, body').animate({scrollTop:target}, 500);
 		event.preventDefault();
 	});
